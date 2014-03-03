7eab5246e686c9ade95873105cd1ff86b598debf
Fellipe Souto Sampaio <fllsouto@gmail.com>
2014-03-02 22:01:55 -0300

    Added model validator and unit tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
+	validates_length_of :title, 
+						within: 5..25, 
+						too_short: "is too short (The minimum is 5 characters)", 
+						too_long: "is too long (The maximum is 25 characters)"
 	validates :image_url, allow_blank: true, format: {
 		with: 	%r{\.(gif|jpg|png)\z}i,
 		message: 'must be a  URL for GIF, JPG, or PNG image.'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
   test "product attributes must not be empty" do 
   	product = Product.new
   	assert product.invalid?
@@ -12,8 +13,8 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product price must be positive" do
   	product = Product.new(title: "my book title",
-													description: "yyy",
-  												image_url: "zzz.jpg")
+						  description: "yyy",
+						  image_url: "zzz.jpg")
 		product.price = -1
 		assert product.invalid?
 		assert_equal "must be greater than or equal to 0.01",
@@ -26,5 +27,56 @@ class ProductTest < ActiveSupport::TestCase
 
 		product.price = 1
 		assert product.valid?
+	end
+
+	def new_product(image_url)
+		Product.new(title: "My Book Title",
+								description: "yyy",
+								price: 1,
+								image_url: image_url)
+	end
+
+	test "image URL ends with one of .gif, .png or .jpg" do
+		correct_array = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    incorrect_array = %w{ fred.doc fred.gif/more fred.gif.more } 
+	
+		correct_array.each do | item | 
+			assert new_product(item).valid? , "#{item} shouldn't be invalid"
+		end
+		incorrect_array.each do | item | 
+			assert new_product(item).invalid? , "#{item} shouldn't be valid"
+		end
+	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(title: products(:ruby).title,
+													description: "yyy",
+													price: 1,
+													image_url: "fred.gif"
+			)
+		assert !product.save
+		assert_equal "has already been taken", product.errors[:title].join('; ')
+	end
+
+	test "product title is too short" do
+		product = Product.new(description: "yyy",
+													price: 1,
+													image_url: "fred.gif"
+			)
+		product.title = "waka"
+		assert product.invalid?
+		assert_equal "is too short (The minimum is 5 characters)",
+			product.errors[:title].join(';')
+	end
+
+	test "product title is too long" do
+		product = Product.new(description: "yyy",
+													price: 1,
+													image_url: "fred.gif"
+			)
+		product.title = "thistitleisveryverylargeandthisisnotgood"
+		assert product.invalid?
+		assert_equal "is too long (The maximum is 25 characters)",
+			product.errors[:title].join(';')
 	end
 end
