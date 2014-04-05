8237010675bb261b8496aea149e542c38fc94d08
Fellipe Souto <fllsouto@gmail.com>
2014-04-05 16:06:36 -0300

    Started iteration D
    I created a new scaffold for cart and a new method 'current_cart' to retrive the cart id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private 
+  	#Placing this method here alows only controler to use it and prevents that method to 
+  	#became an action controller
+  	def current_cart 
+  		Cart.find(session[:cart_id]) #search in database, session works like a hash table
+  	rescue ActionRecord::RecordNotFound #This rescue is for when the cart isn't found
+  		cart = Cart.create
+  		session[:cart_id] = cart_id
+  		cart
+  	end
 end
